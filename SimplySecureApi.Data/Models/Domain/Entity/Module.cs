c240592f1cef19ea221b001a81ac6e1899fc3465
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-23 21:12:00 -0500

    Added IsSilentAlarm property

--- SimplySecureApi.Data/Models/Domain/Entity/Module.cs
+++ SimplySecureApi.Data/Models/Domain/Entity/Module.cs
@@ -1,11 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace SimplySecureApi.Data.Models.Domain.Entity
 {
     public class Module : Auditable
     {
         [JsonProperty("name")]
-        public bool Name { get; set; }
+        public string Name { get; set; }
 
         [JsonProperty("state")]
         public bool State { get; set; }
@@ -13,7 +14,14 @@ namespace SimplySecureApi.Data.Models.Domain.Entity
         [JsonProperty("armed")]
         public bool Armed { get; set; }
 
+        [JsonProperty("isMotionDetector")]
+        public bool IsMotionDetector { get; set; }
+
+        [JsonProperty("isSilentAlarm")]
+        public bool IsSilentAlarm { get; set; }
+
         [JsonProperty("triggered")]
+        [ScaffoldColumn(false)]
         public bool Triggered { get; set; }
     }
 }
