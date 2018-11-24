f132989bcb7ca6065e3a85f63dbf307161067e17
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-24 12:01:29 -0500

    Included Locations in DataContext

--- SimplySecureApi.Data/DataContext/SimplySecureDataContext.cs
+++ SimplySecureApi.Data/DataContext/SimplySecureDataContext.cs
@@ -8,15 +8,19 @@ namespace SimplySecureApi.Data.DataContext
 {
     public class SimplySecureDataContext : IdentityDbContext<ApplicationUser>
     {
+        //TODO use repository pattern. External libraries should not have access to members of data context
         public DbSet<Module> Modules { get; set; }
+
+        //TODO use repository pattern. External libraries should not have access to members of data context
+        public DbSet<ModuleStateChange> ModuleStateChanges { get; set; }
 
         internal DbSet<BootMessage> BootMessages { get; set; }
 
         internal DbSet<TriggeredModule> TriggeredModules { get; set; }
 
-        internal DbSet<ModuleStateChange> ModuleStateChanges { get; set; }
-
         internal DbSet<ArmedModule> ArmedModules { get; set; }
+
+        internal DbSet<Location> Locations { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
