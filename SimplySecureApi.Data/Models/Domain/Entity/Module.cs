41d39d345386542686eceb0594dda9ca9556bf24
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-24 12:03:24 -0500

    Removed properties from Module that were moved to Location Class

--- SimplySecureApi.Data/Models/Domain/Entity/Module.cs
+++ SimplySecureApi.Data/Models/Domain/Entity/Module.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
 using Newtonsoft.Json;
 
 namespace SimplySecureApi.Data.Models.Domain.Entity
@@ -11,17 +11,12 @@ namespace SimplySecureApi.Data.Models.Domain.Entity
         [JsonProperty("state")]
         public bool State { get; set; }
 
-        [JsonProperty("armed")]
-        public bool Armed { get; set; }
-
         [JsonProperty("isMotionDetector")]
         public bool IsMotionDetector { get; set; }
 
-        [JsonProperty("isSilentAlarm")]
-        public bool IsSilentAlarm { get; set; }
+        [JsonProperty("locationId")]
+        public Guid LocationId { get; set; }
 
-        [JsonProperty("triggered")]
-        [ScaffoldColumn(false)]
-        public bool Triggered { get; set; }
+        public virtual Location Location { get; set; }
     }
 }
