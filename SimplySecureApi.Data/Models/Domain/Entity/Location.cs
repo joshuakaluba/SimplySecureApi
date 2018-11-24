6ce2de443a10fafc814e461be04a901cda21403c
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-24 12:02:58 -0500

    Created Location class

---
+++ SimplySecureApi.Data/Models/Domain/Entity/Location.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimplySecureApi.Data.Models.Domain.Entity
+{
+    public class Location : Auditable
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("armed")]
+        public bool Armed { get; set; }
+
+        [JsonProperty("isSilentAlarm")]
+        public bool IsSilentAlarm { get; set; }
+
+        [JsonProperty("triggered")]
+        [ScaffoldColumn(false)]
+        public bool Triggered { get; set; }
+    }
+}
