849b5f7dd8869ef2d2e44d1432a9fc3682f12f94
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-25 19:31:50 -0500

    Created LocationsController for CRUD on Locations

---
+++ SimplySecureApi.Web/Controllers/LocationsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SimplySecureApi.Data.DataContext;
+using SimplySecureApi.Data.Models.Domain.Entity;
+
+namespace SimplySecureApi.Web.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly SimplySecureDataContext _context;
+
+        public LocationsController(SimplySecureDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Locations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Locations.ToListAsync());
+        }
+
+        // GET: Locations/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Armed,IsSilentAlarm,Id")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                location.Id = Guid.NewGuid();
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Name,Armed,IsSilentAlarm,Id")] Location location)
+        {
+            if (id != location.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(location.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LocationExists(Guid id)
+        {
+            return _context.Locations.Any(e => e.Id == id);
+        }
+    }
+}
