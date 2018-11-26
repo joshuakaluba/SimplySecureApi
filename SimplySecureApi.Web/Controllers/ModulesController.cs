8791e339ce210dd7d63bc051c1bded144968fe7e
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-25 19:32:35 -0500

    Created Autogenerated ModulesController to handle changes in the way armed state is managed

---
+++ SimplySecureApi.Web/Controllers/ModulesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SimplySecureApi.Data.DataContext;
+using SimplySecureApi.Data.Models.Domain.Entity;
+
+namespace SimplySecureApi.Web.Controllers
+{
+    public class ModulesController : Controller
+    {
+        private readonly SimplySecureDataContext _context;
+
+        public ModulesController(SimplySecureDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Modules
+        public async Task<IActionResult> Index()
+        {
+            var simplySecureDataContext = _context.Modules.Include(m => m.Location);
+            return View(await simplySecureDataContext.ToListAsync());
+        }
+
+        // GET: Modules/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules
+                .Include(m => m.Location)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // GET: Modules/Create
+        public IActionResult Create()
+        {
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id");
+            return View();
+        }
+
+        // POST: Modules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,State,IsMotionDetector,LocationId,Id")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                @module.Id = Guid.NewGuid();
+                _context.Add(@module);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id", module.Name);
+            return View(@module);
+        }
+
+        // GET: Modules/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules.FindAsync(id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id", module.LocationId);
+            return View(@module);
+        }
+
+        // POST: Modules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Name,State,IsMotionDetector,LocationId,Id")] Module module)
+        {
+            if (id != @module.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@module);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModuleExists(module.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id",module.Name);
+            return View(@module);
+        }
+
+        // GET: Modules/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules
+                .Include(m => m.Location)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // POST: Modules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var @module = await _context.Modules.FindAsync(id);
+            _context.Modules.Remove(@module);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModuleExists(Guid id)
+        {
+            return _context.Modules.Any(e => e.Id == id);
+        }
+    }
+}
