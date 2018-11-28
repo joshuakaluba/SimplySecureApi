18783ab08aada9ebf8efea5589f2c96c4e63e317
Joshua Kaluba <joshkaluba@gmail.com>
2018-11-28 17:05:00 -0500

    Created LocationTriggeringService

---
+++ SimplySecureApi.Data/Services/LocationTriggeringService.cs
@@ -0,0 +1,31 @@
+﻿using SimplySecureApi.Data.DataAccessLayer.Locations;
+using SimplySecureApi.Data.Models.Domain.Entity;
+using SimplySecureApi.Data.Models.Response;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.Services
+{
+    public class LocationTriggeringService
+    {
+        public static async Task<ModuleResponse> ProcessLocationTriggered(ILocationRepository locationRepository, Location location)
+        {
+            var triggeredFlag = false;
+
+            if (location.Armed)
+            {
+                await locationRepository.TriggerLocation(location);
+
+                triggeredFlag = true;
+            }
+
+            var moduleResponse = new ModuleResponse
+            {
+                Armed = location.Armed,
+
+                Triggered = triggeredFlag && !location.IsSilentAlarm
+            };
+
+            return moduleResponse;
+        }
+    }
+}
