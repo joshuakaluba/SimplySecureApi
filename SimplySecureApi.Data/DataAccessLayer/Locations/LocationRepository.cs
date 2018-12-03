28a73c70bd4847c0e7c9af78d947e5c0db5fb2e4
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-02 19:25:13 -0500

    Added implementation for ILocationRespository

---
+++ SimplySecureApi.Data/DataAccessLayer/Locations/LocationRepository.cs
@@ -0,0 +1,21 @@
+﻿using SimplySecureApi.Data.DataContext;
+using SimplySecureApi.Data.Models.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.DataAccessLayer.Locations
+{
+    public class LocationRepository : BaseRepository, ILocationRepository
+    {
+        public async Task TriggerLocation(Location location)
+        {
+            using (DataContext = new SimplySecureDataContext())
+            {
+                location.Triggered = true;
+
+                DataContext.Locations.Update(location);
+
+                await DataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
