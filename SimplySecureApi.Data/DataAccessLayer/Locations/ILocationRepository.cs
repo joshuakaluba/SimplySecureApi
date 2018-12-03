9acd35dae827a8d9592fc5d5d2790593363e3ec6
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-02 19:24:51 -0500

    Added ILocationRepository

---
+++ SimplySecureApi.Data/DataAccessLayer/Locations/ILocationRepository.cs
@@ -0,0 +1,10 @@
+﻿using SimplySecureApi.Data.Models.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.DataAccessLayer.Locations
+{
+    public interface ILocationRepository
+    {
+        Task TriggerLocation(Location location);
+    }
+}
