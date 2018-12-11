9e69d11268172ba2abfc17f7f8e4722aad2dfabf
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-10 22:59:05 -0500

    Added error handling and private respository variables

--- SimplySecureApi.Web/Controllers/HomeController.cs
+++ SimplySecureApi.Web/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SimplySecureApi.Data.DataAccessLayer.ModuleEvents;
 using SimplySecureApi.Data.Models.Authentication;
 using SimplySecureApi.Data.Models.Domain.ViewModels;
+using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SimplySecureApi.Web.Controllers
 {
     public class HomeController : BaseController
     {
-        public HomeController(UserManager<ApplicationUser> userManager)
+        private readonly IModuleEventRepository _moduleEventRepository;
+
+        public HomeController(UserManager<ApplicationUser> userManager, IModuleEventRepository moduleEventRepository)
             : base(userManager)
         {
+            _moduleEventRepository = moduleEventRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        public async Task<IActionResult> ModuleEvents()
+        {
+            try
+            {
+                var modules
+                    = await _moduleEventRepository.GetModuleEvents();
+
+                return View(modules);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         public IActionResult Error()
