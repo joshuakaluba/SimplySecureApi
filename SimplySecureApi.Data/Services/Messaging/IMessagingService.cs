df4c07ba810d6fe483e82952e717b705d36e6cd4
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-15 17:16:19 -0500

    Created messaging implementation and messaging service

---
+++ SimplySecureApi.Data/Services/Messaging/IMessagingService.cs
@@ -0,0 +1,12 @@
+﻿using SimplySecureApi.Data.Models.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.Services.Messaging
+{
+    public interface IMessagingService
+    {
+        Task SendModuleTriggeredMessage(Module module);
+
+        Task SendModuleOfflineMessage(Module module);
+    }
+}
---
+++ SimplySecureApi.Data/Services/Messaging/MessagingService.cs
@@ -0,0 +1,38 @@
+﻿using SimplySecureApi.Common.Messaging;
+using SimplySecureApi.Data.Models.Domain.Entity;
+using SimplySecureApi.Data.Models.Static;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.Services.Messaging
+{
+    public class MessagingService : IMessagingService
+    {
+        public async Task SendModuleTriggeredMessage(Module module)
+        {
+            var sid = ApplicationConfig.TwilioAccountSId;
+            var authToken = ApplicationConfig.TwilioAuthenticationToken;
+            var sender = ApplicationConfig.TwilioSenderPhoneNumber;
+            var recipient = ApplicationConfig.TwilioRecipientPhoneNumber;
+
+            var content = @"An alarm has been triggered at location: " + $"{module.Location.Name}, for module: {module.Name}.";
+
+            var twilioMessage = new TwilioMessage(sid, authToken, sender);
+
+            await twilioMessage.Send(recipient, content);
+        }
+
+        public async Task SendModuleOfflineMessage(Module module)
+        {
+            var sid = ApplicationConfig.TwilioAccountSId;
+            var authToken = ApplicationConfig.TwilioAuthenticationToken;
+            var sender = ApplicationConfig.TwilioSenderPhoneNumber;
+            var recipient = ApplicationConfig.TwilioRecipientPhoneNumber;
+
+            var content = $"Module: {module.Name} at location: " + $"{module.Location.Name} has changed to 'Offline'";
+
+            var twilioMessage = new TwilioMessage(sid, authToken, sender);
+
+            await twilioMessage.Send(recipient, content);
+        }
+    }
+}
