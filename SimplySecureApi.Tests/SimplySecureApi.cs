93595d619c3579de59e0dc79c500503392d5d438
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-15 17:19:40 -0500

    Created tests for twilio messaging service

--- SimplySecureApi.Tests/SimplySecureApi.cs
+++ SimplySecureApi.Tests/SimplySecureApi.cs
@@ -1,4 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplySecureApi.Common.Messaging;
+using SimplySecureApi.Data.Models.Domain.Entity;
+using SimplySecureApi.Data.Models.Static;
+using SimplySecureApi.Data.Services.Messaging;
 
 namespace SimplySecureApi.Tests
 {
@@ -6,8 +10,47 @@ namespace SimplySecureApi.Tests
     public class SimplySecureApi
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(System.AggregateException))]
+        public void ModuleInValidLocation()
         {
+            var module = new Module { Location = null };
+
+            var messagingService = new MessagingService();
+
+            messagingService.SendModuleTriggeredMessage(module).Wait();
+        }
+
+        [TestMethod]
+        public void ModuleValidLocation()
+        {
+            var location = new Location
+            {
+                Name = "Test Location",
+                Armed = true,
+                IsSilentAlarm = false,
+                Triggered = false
+            };
+
+            var module = new Module
+            {
+                Name = "Main Door",
+                Location = location,
+                State = false
+            };
+
+            var messagingService = new MessagingService();
+
+            messagingService.SendModuleTriggeredMessage(module).Wait();
+        }
+
+        [TestMethod]
+        public void SendTwilioMessage()
+        {
+            var twilioMessage
+                = new TwilioMessage(ApplicationConfig.TwilioAccountSId,
+                ApplicationConfig.TwilioAuthenticationToken, ApplicationConfig.TwilioSenderPhoneNumber);
+
+            twilioMessage.Send(ApplicationConfig.TwilioRecipientPhoneNumber, "Test Message from tests").Wait();
         }
     }
 }
