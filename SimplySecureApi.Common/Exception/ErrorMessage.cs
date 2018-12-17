c72036a18c0c81350512a1ec8bd593c0f9e54150
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-17 17:57:28 -0500

    Created ErrorMessage model

---
+++ SimplySecureApi.Common/Exception/ErrorMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimplySecureApi.Common.Exception
+{
+    public class ErrorMessage
+    {
+        public ErrorMessage(string message)
+        {
+            this.Message = message;
+        }
+
+        public ErrorMessage(System.Exception exception)
+        {
+            this.Message = exception.Message;
+        }
+
+        public String Message { get; set; }
+    }
+}
