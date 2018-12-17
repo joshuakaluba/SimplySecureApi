620eda9b75e5f44212e54f4a1e8d7973db66808d
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-17 17:56:28 -0500

    Created String Extensions class

---
+++ SimplySecureApi.Common/Extensions/Strings/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace SimplySecureApi.Common.Extensions.Strings
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string title)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(title);
+        }
+    }
+}
