222ee955a15f08c158b58777d6a07fb29f851570
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-16 19:38:15 -0500

    Created Twilo messaging service

---
+++ SimplySecureApi.Common/Messaging/TwilioMessage.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace SimplySecureApi.Common.Messaging
+{
+    public class TwilioMessage
+    {
+        private string _accountSid = "";
+        private string _authenticationToken = "";
+        private string _senderPhoneNumber = "";
+
+        public TwilioMessage(string accountSid, string authenticationToken, string senderPhoneNumber)
+        {
+            _accountSid = accountSid;
+
+            _authenticationToken = authenticationToken;
+
+            _senderPhoneNumber = senderPhoneNumber;
+        }
+
+        public async Task Send(string recipientPhoneNumber, string messageContent)
+        {
+            TwilioClient.Init(_accountSid, _authenticationToken);
+
+            var messageOptions = new CreateMessageOptions(new PhoneNumber(recipientPhoneNumber));
+
+            messageOptions.From = new PhoneNumber(_senderPhoneNumber);
+
+            messageOptions.Body = messageContent;
+
+            var message = await MessageResource.CreateAsync(messageOptions);
+        }
+    }
+}
