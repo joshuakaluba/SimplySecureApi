ef5a9feeb90a3c9db2c755e6da7226a0b576e788
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-16 19:36:47 -0500

    Created User Validator

---
+++ SimplySecureApi.Services/Authentication/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimplySecureApi.Data.Models.Authentication;
+
+namespace SimplySecureApi.Services.Authentication
+{
+    public static class UserValidator
+    {
+        public static bool Validate(ApplicationUser user)
+        {
+            return user.Active;
+        }
+    }
+}
