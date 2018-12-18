b0aaa3bb9e2f7b687bcc0c3ab1f1be846285bec7
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-18 08:30:24 -0500

    Creayed CORS middleware

---
+++ SimplySecureApi.Web/MiddleWare/MaintainCorsHeadersMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SimplySecureApi.Web.MiddleWare
+{
+    public class MaintainCorsHeadersMiddleware
+    {
+        public MaintainCorsHeadersMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        private readonly RequestDelegate _next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // Find and hold onto any CORS related headers ...
+            var corsHeaders = new HeaderDictionary();
+            foreach (var pair in httpContext.Response.Headers)
+            {
+                if (!pair.Key.ToLower().StartsWith("access-control-")) { continue; } // Not CORS related
+                corsHeaders[pair.Key] = pair.Value;
+            }
+
+            // Bind to the OnStarting event so that we can make sure these CORS headers are still included going to the client
+            httpContext.Response.OnStarting(o => {
+                var ctx = (HttpContext)o;
+                var headers = ctx.Response.Headers;
+                // Ensure all CORS headers remain or else add them back in ...
+                foreach (var pair in corsHeaders)
+                {
+                    if (headers.ContainsKey(pair.Key)) { continue; } // Still there!
+                    headers.Add(pair.Key, pair.Value);
+                }
+                return Task.CompletedTask;
+            }, httpContext);
+
+            // Call the pipeline ...
+            await _next(httpContext);
+        }
+    }
+}
