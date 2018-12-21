0ef12587d8e4edfa5185ea861ed36fc459a9177b
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-21 11:24:39 -0500

    Created LocationViewModel

---
+++ SimplySecureApi.Data/Models/Domain/ViewModels/LocationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SimplySecureApi.Data.Models.Domain.ViewModels
+{
+    public class LocationViewModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("armed")]
+        public bool Armed { get; set; }
+
+        [JsonProperty("isSilentAlarm")]
+        public bool IsSilentAlarm { get; set; } = false;
+
+        [JsonProperty("triggered")]
+        public bool Triggered { get; set; }
+
+        public bool Active { get; set; } = true;
+    }
+}
