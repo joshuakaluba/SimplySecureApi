86b7a4635109fc5836754924df9a8bb1bdf31c9d
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-24 00:19:35 -0500

    Updated copy to clipboard functionality:

--- SimplySecureApi.Web/wwwroot/js/Core.js
+++ SimplySecureApi.Web/wwwroot/js/Core.js
@@ -134,7 +134,42 @@
                 }
             }
             return "";
-        };        
+        }; 
+
+        this.CopyToClipboard = function (text)
+        {
+            try {
+                if (window.clipboardData)
+                {
+                    window.clipboardData.setData('Text', text);
+                }
+                else
+                {
+                    var textArea = document.createElement("textarea");
+
+                    textArea.style.position = 'fixed';
+                    textArea.style.top = -100;
+                    textArea.style.left = -100;
+                    textArea.style.width = '2em';
+                    textArea.style.height = '2em';
+                    textArea.style.padding = 0;
+                    textArea.style.border = 'none';
+                    textArea.style.outline = 'none';
+                    textArea.style.boxShadow = 'none';
+                    textArea.style.background = 'transparent';
+                    textArea.value = text;
+
+                    document.body.appendChild(textArea);
+                    textArea.select();
+                    document.execCommand('copy');
+                    document.body.removeChild(textArea);
+                }
+            }
+            catch (ex)
+            {
+                Core.ShowException('Copy to clipboard is not supported in this browser.');
+            }
+        };
 
     }
     catch ( ex )
