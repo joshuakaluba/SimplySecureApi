2b205dfd3a7d8684cc1716db2f6dda3efb8ee9a0
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-25 01:01:40 -0500

    Moved TokenOption to authentication namespace

---
+++ SimplySecureApi.Data/Models/Authentication/TokenOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SimplySecureApi.Data.Models.Authentication
+{
+    public class TokenOptions
+    {
+        public string Issuer { get; set; }
+        public string Key { get; set; }
+    }
+}
