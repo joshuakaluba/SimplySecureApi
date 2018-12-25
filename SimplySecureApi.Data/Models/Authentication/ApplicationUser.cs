0f7f30946f7a08d1cc088a82356b5cf0477709c3
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-25 01:00:35 -0500

    added fullname to application user class

--- SimplySecureApi.Data/Models/Authentication/ApplicationUser.cs
+++ SimplySecureApi.Data/Models/Authentication/ApplicationUser.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace SimplySecureApi.Data.Models.Authentication
 {
     public class ApplicationUser : IdentityUser
     {
-        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        [DataMember]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Account Active")]
+        public bool Active { get; set; } = true;
     }
 }
