468e1059b9df6923db0ff9bef549e4d3f78e006d
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-25 01:01:10 -0500

    Moved token to authentication namespace

---
+++ SimplySecureApi.Data/Models/Authentication/Token.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SimplySecureApi.Data.Models.Authentication
+{
+    public class Token
+    {
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("fullName")]
+        public string FullName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("expiryDate")]
+        public System.DateTime ExpiryDate { get; set; }
+
+        [JsonProperty("dateUserCreated")]
+        public System.DateTime DateUserCreated { get; set; }
+    }
+}
