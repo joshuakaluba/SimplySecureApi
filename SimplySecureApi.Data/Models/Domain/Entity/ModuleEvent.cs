68f5f3d92737e8a7127b2fcfb034e66b3b22a6ab
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-25 01:02:28 -0500

    Created ModuleEvent class

---
+++ SimplySecureApi.Data/Models/Domain/Entity/ModuleEvent.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimplySecureApi.Data.Models.Domain.Entity
+{
+    public class ModuleEvent : Auditable
+    {
+        public ModuleEvent()
+        {
+        }
+
+        public ModuleEvent(Guid moduleId, bool state)
+        {
+            ModuleId = moduleId;
+
+            State = state;
+        }
+
+        [Display(Name = "State")]
+        public string StateDisplayed
+        {
+            get
+            {
+                if (Module == null)
+                {
+                    return State.ToString();
+                }
+
+                return 
+                    Module.IsMotionDetector ? State ? "Motion detected" :
+                    "No motion detected" :
+                    State ? "Door Closed" : "Door Opened";
+            }
+        }
+
+        [Display(Name = "State")]
+        [JsonProperty("state")]
+        public bool State { get; set; }
+
+        [Display(Name = "Module")]
+        [JsonProperty("moduleId")]
+        public Guid ModuleId { get; set; }
+
+        public virtual Module Module { get; set; }
+    }
+}
