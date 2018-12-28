91df856257418613deec9aec2f0cc1bb44c3da49
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-27 21:04:18 -0500

    Added roles

--- SimplySecureApi.Data/Initialization/DataContextInitializer.cs
+++ SimplySecureApi.Data/Initialization/DataContextInitializer.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SimplySecureApi.Data.DataContext;
+using SimplySecureApi.Data.Models.Domain.Entity;
 using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SimplySecureApi.Data.Models.Authentication;
 
 namespace SimplySecureApi.Data.Initialization
 {
     public static class DataContextInitializer
     {
+        public static Guid DefaultLocationGuid => new Guid("14b06356-1a28-486a-9c18-2a7b0543d276");
+
+        public static Claim DefaultUserClaim => new Claim("DefaultUserClaim", "DefaultUserAuthorization");
+
+        public static string AdministratorRole => "Administrator";
+
+        public static string UserRole => "User";
+
+        private static readonly string[] Roles = new string[] { AdministratorRole, UserRole };
+
         public static async Task Seed(IServiceProvider serviceProvider)
         {
             using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
@@ -18,6 +32,37 @@ namespace SimplySecureApi.Data.Initialization
                 if (context.Database.GetPendingMigrations().Any())
                 {
                     await context.Database.MigrateAsync();
+
+                    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    var userManger = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    foreach (var role in Roles)
+                    {
+                        if (!await roleManager.RoleExistsAsync(role))
+                        {
+                            await roleManager.CreateAsync(new IdentityRole(role));
+                        }
+                    }
+
+                    var defaultLocation = await context.Locations.FirstOrDefaultAsync();
+
+                    if (defaultLocation == null)
+                    {
+                        defaultLocation = new Location
+                        {
+                            Id = DataContextInitializer.DefaultLocationGuid,
+                            Name = "Default Location",
+                            Active = true,
+                            IsSilentAlarm = false,
+                            Armed = false,
+                            Triggered = false
+                        };
+
+                        context.Locations.Add(defaultLocation);
+
+                        await context.SaveChangesAsync();
+                    }
                 }
             }
         }
