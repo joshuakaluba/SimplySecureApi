a1f98cf23ab43a1f70a0d93827b588241950d45f
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-28 15:03:52 -0500

    Created IUserRepository

---
+++ SimplySecureApi.Data/DataAccessLayer/Authentication/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SimplySecureApi.Data.Models.Authentication;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.DataAccessLayer.Authentication
+{
+    public interface IUserRepository
+    {
+        Task<IdentityResult> RegisterNewUser(UserManager<ApplicationUser> userManager, ApplicationUser user, string password);
+    }
+}
