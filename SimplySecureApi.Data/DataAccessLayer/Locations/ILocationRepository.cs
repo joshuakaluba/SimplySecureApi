518bb7da6ee5b58ad065861abf4991556abf28fd
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-28 15:04:46 -0500

    Created UpdateLocation method

--- SimplySecureApi.Data/DataAccessLayer/Locations/ILocationRepository.cs
+++ SimplySecureApi.Data/DataAccessLayer/Locations/ILocationRepository.cs
@@ -1,10 +1,26 @@
 ﻿using SimplySecureApi.Data.Models.Domain.Entity;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SimplySecureApi.Data.DataAccessLayer.Locations
 {
     public interface ILocationRepository
     {
+        Task CreateLocation(Location location);
+
         Task TriggerLocation(Location location);
+
+        Task ArmLocation(Location location);
+
+        Task DisarmLocation(Location location);
+
+        Task<Location> FindLocationById(Guid id);
+
+        Task DeleteLocation(Location location);
+
+        Task<List<Location>> GetLocations();
+
+        Task UpdateLocation(Location location);
     }
 }
