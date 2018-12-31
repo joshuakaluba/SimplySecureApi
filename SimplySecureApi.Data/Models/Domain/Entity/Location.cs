291c0a19ab6e0fd81dee9fada1d9e0b837ae0356
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-31 10:37:12 -0500

    Formatting changes

--- SimplySecureApi.Data/Models/Domain/Entity/Location.cs
+++ SimplySecureApi.Data/Models/Domain/Entity/Location.cs
@@ -5,17 +5,26 @@ namespace SimplySecureApi.Data.Models.Domain.Entity
 {
     public class Location : Auditable
     {
+        [Display(Name = "Location Name")]
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        [Display(Name = "Armed")]
         [JsonProperty("armed")]
         public bool Armed { get; set; }
 
+        [Display(Name = "Silent Alarm")]
         [JsonProperty("isSilentAlarm")]
         public bool IsSilentAlarm { get; set; }
 
+        [Display(Name = "Triggered")]
         [JsonProperty("triggered")]
         [ScaffoldColumn(false)]
         public bool Triggered { get; set; }
+
+        public bool IsNotTriggered => !Triggered;
+
+        [JsonProperty("active")]
+        public bool Active { get; set; } = true;
     }
 }
