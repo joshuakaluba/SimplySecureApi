eb1b93f465363a3ecc77adea84eddf61ea5e0e70
Joshua Kaluba <joshkaluba@gmail.com>
2019-02-01 15:13:39 -0500

    Created LocationActionEventsRepository

---
+++ SimplySecureApi.Data/DataAccessLayer/LocationActionEvents/ILocationActionEventsRepository.cs
@@ -0,0 +1,13 @@
+﻿using SimplySecureApi.Data.Models.Domain.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Data.DataAccessLayer.LocationActionEvents
+{
+    public interface ILocationActionEventsRepository
+    {
+        Task SaveLocationActionEvent(LocationActionEvent locationActionEvent);
+
+        Task<List<LocationActionEvent>> GetLocationActionEventsByLocation(Location location);
+    }
+}
---
+++ SimplySecureApi.Data/DataAccessLayer/LocationActionEvents/LocationActionEventsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimplySecureApi.Data.DataContext;
+using SimplySecureApi.Data.Models.Domain.Entity;
+
+namespace SimplySecureApi.Data.DataAccessLayer.LocationActionEvents
+{
+    public class LocationActionEventsRepository : BaseRepository, ILocationActionEventsRepository
+    {
+        public async Task SaveLocationActionEvent(LocationActionEvent locationActionEvent)
+        {
+            using (DataContext = new SimplySecureDataContext())
+            {
+                DataContext.LocationActionEvents.Add(locationActionEvent);
+
+                await DataContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<LocationActionEvent>> GetLocationActionEventsByLocation(Location location)
+        {
+            using (DataContext = new SimplySecureDataContext())
+            {
+                var actionEvents 
+                    = await DataContext.LocationActionEvents
+                        .Where(a => a.LocationId == location.Id)
+                            .Include(e=>e.ApplicationUser)
+                                .OrderByDescending(l => l.DateCreated)
+                                    .ToListAsync();
+
+                return actionEvents;
+            }
+        }
+    }
+}
