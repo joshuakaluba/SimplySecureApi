d93ff893ebced1b97c905c80da322f602d7d0796
Joshua Kaluba <joshkaluba@gmail.com>
2019-02-01 15:18:18 -0500

    Added ability to send push notifications

--- SimplySecureApi.Data/Services/Messaging/MessagingService.cs
+++ SimplySecureApi.Data/Services/Messaging/MessagingService.cs
@@ -1,38 +1,97 @@
-﻿using SimplySecureApi.Common.Messaging;
+﻿using Floxdc.ExponentServerSdk;
+using SimplySecureApi.Common.Messaging;
+using SimplySecureApi.Data.DataAccessLayer.LocationUsers;
+using SimplySecureApi.Data.DataAccessLayer.PushNotificationTokens;
 using SimplySecureApi.Data.Models.Domain.Entity;
 using SimplySecureApi.Data.Models.Static;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Module = SimplySecureApi.Data.Models.Domain.Entity.Module;
 
 namespace SimplySecureApi.Data.Services.Messaging
 {
     public class MessagingService : IMessagingService
     {
-        public async Task SendModuleTriggeredMessage(Module module)
+        private readonly ILocationUsersRepository _locationUsersRepository;
+        private readonly IPushNotificationTokensRepository _pushNotificationTokensRepository;
+
+        public MessagingService()
+        {
+            _locationUsersRepository = new LocationUsersRepository();
+
+            _pushNotificationTokensRepository = new PushNotificationTokensRepository();
+        }
+
+        private async Task SendPushNotification(Location location, string title, string content)
+        {
+            var locationUsers = await _locationUsersRepository.GetLocationUsers(location);
+
+            var applicationUsers = locationUsers.Select(m => m.ApplicationUser).ToList();
+
+            var pushNotificationTokens =
+                await _pushNotificationTokensRepository.GetLocationsPushNotificationTokens(applicationUsers);
+
+            foreach (var token in pushNotificationTokens)
+            {
+                await SendExpoMessage(token.Token, title, content);
+            }
+        }
+
+        private async Task SendExpoMessage(string token, string title, string message)
+        {
+            try
+            {
+                var client = new PushClient();
+                var notification = new PushMessage(token, title: title, body: message);
+                await client.Publish(notification);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private async Task SendTwilioMessage(string content)
         {
             var sid = ApplicationConfig.TwilioAccountSId;
             var authToken = ApplicationConfig.TwilioAuthenticationToken;
             var sender = ApplicationConfig.TwilioSenderPhoneNumber;
             var recipient = ApplicationConfig.TwilioRecipientPhoneNumber;
-
-            var content = @"An alarm has been triggered at location: " + $"{module.Location.Name}, for module: {module.Name}.";
 
             var twilioMessage = new TwilioMessage(sid, authToken, sender);
 
             await twilioMessage.Send(recipient, content);
         }
 
+        public async Task SendModuleTriggeredMessage(Module module)
+        {
+            try
+            {
+                var content = @"An alarm has been triggered at location: " + $"{module.Location.Name}, for module: {module.Name}.";
+                await SendTwilioMessage(content);
+
+                await SendPushNotification(module.Location, "Alarm Triggered", content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
         public async Task SendModuleOfflineMessage(Module module)
         {
-            var sid = ApplicationConfig.TwilioAccountSId;
-            var authToken = ApplicationConfig.TwilioAuthenticationToken;
-            var sender = ApplicationConfig.TwilioSenderPhoneNumber;
-            var recipient = ApplicationConfig.TwilioRecipientPhoneNumber;
+            try
+            {
+                var content = $"Module: {module.Name} at location: " + $"{module.Location.Name} has changed to 'Offline'";
+                await SendTwilioMessage(content);
 
-            var content = $"Module: {module.Name} at location: " + $"{module.Location.Name} has changed to 'Offline'";
-
-            var twilioMessage = new TwilioMessage(sid, authToken, sender);
-
-            await twilioMessage.Send(recipient, content);
+                await SendPushNotification(module.Location, "Module Office", content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
