85f1387dc6f9706e19c99d1b3c6ac22442d950e0
Joshua Kaluba <joshkaluba@gmail.com>
2019-02-23 19:01:39 -0500

    Created LocationAction controller

---
+++ SimplySecureApi.Web/Controllers/LocationActionEventsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimplySecureApi.Data.DataAccessLayer.LocationActionEvents;
+using SimplySecureApi.Data.DataAccessLayer.Locations;
+using SimplySecureApi.Data.Models.Authentication;
+using SimplySecureApi.Data.Models.Response;
+using System;
+using System.Threading.Tasks;
+
+namespace SimplySecureApi.Web.Controllers
+{
+    public class LocationActionEventsController : BaseController<LocationActionEventsController>
+    {
+        private readonly ILocationRepository _locationRepository;
+        private readonly ILocationActionEventsRepository _locationActionEventsRepository;
+
+        public LocationActionEventsController(UserManager<ApplicationUser> userManager,
+            ILocationActionEventsRepository locationActionEventsRepository,
+            ILocationRepository locationRepository,
+            ILogger<LocationActionEventsController> logger)
+            : base(userManager, logger)
+        {
+            _locationRepository = locationRepository;
+            _locationActionEventsRepository = locationActionEventsRepository;
+        }
+
+        //LocationActionEvents/GetLocationHistory/{id}
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> GetLocationHistory(Guid id)
+        {
+            try
+            {
+                var location
+                    = await _locationRepository.GetLocationById(id);
+
+                var user = await GetUser();
+
+                await _locationRepository.ValidateLocationForUser(user, location);
+
+                var events
+                    = await _locationActionEventsRepository.GetLocationActionEventsByLocation(location);
+
+                return Ok(events);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+
+                return BadRequest(new ErrorResponse(ex));
+            }
+        }
+    }
+}
