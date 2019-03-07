f91cfc91b7fc0a37618113693ab6678f29c3dcf5
Joshua Kaluba <joshkaluba@gmail.com>
2019-03-07 14:51:05 -0500

    Created PanicViewModel

---
+++ SimplySecureApi.Data/Models/Domain/ViewModels/PanicViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SimplySecureApi.Data.Models.Domain.ViewModels
+{
+    public class PanicViewModel
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [JsonProperty("dateCreated")]
+        public virtual DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
