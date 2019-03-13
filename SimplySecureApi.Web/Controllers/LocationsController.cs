b0043837b9a043ac1de44690df9f0208197c092c
Joshua Kaluba <joshkaluba@gmail.com>
2019-03-12 21:42:13 -0400

    Updated Location Controller

--- SimplySecureApi.Web/Controllers/LocationsController.cs
+++ SimplySecureApi.Web/Controllers/LocationsController.cs
@@ -1,171 +1,179 @@
-﻿//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using SimplySecureApi.Data.DataAccessLayer.Locations;
-//using SimplySecureApi.Data.Models.Authentication;
-//using SimplySecureApi.Data.Models.Domain.Entity;
-//using SimplySecureApi.Data.Models.Response;
-//using System;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Authorization;
-//using SimplySecureApi.Data.DataAccessLayer.LocationUsers;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimplySecureApi.Common.Exception;
+using SimplySecureApi.Common.Extensions.Strings;
+using SimplySecureApi.Data.DataAccessLayer.LocationActionEvents;
+using SimplySecureApi.Data.DataAccessLayer.Locations;
+using SimplySecureApi.Data.DataAccessLayer.LocationUsers;
+using SimplySecureApi.Data.Models.Authentication;
+using SimplySecureApi.Data.Models.Domain.Entity;
+using SimplySecureApi.Data.Models.Domain.ViewModels;
+using SimplySecureApi.Data.Models.Response;
+using SimplySecureApi.Data.Models.Static;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace SimplySecureApi.Web.Controllers
-//{
-//    [Authorize(Roles = "User")]
-//    public class LocationsController : BaseController
-//    {
-//        private readonly ILocationRepository _locationRepository;
-//        private readonly ILocationUsersRepository _locationUsersRepository;
+namespace SimplySecureApi.Web.Controllers
+{
+    public class LocationsController : BaseController<LocationsController>
+    {
+        private readonly ILocationRepository _locationRepository;
+        private readonly ILocationUsersRepository _locationUsersRepository;
+        private readonly ILocationActionEventsRepository _locationActionEventsRepository;
 
-//        public LocationsController(UserManager<ApplicationUser> userManager, 
-//            ILocationRepository locationRepository,
-//            ILocationUsersRepository locationUsersRepository)
-//            : base(userManager)
-//        {
-//            _locationRepository = locationRepository;
-//            _locationUsersRepository = locationUsersRepository;
-//        }
+        public LocationsController(UserManager<ApplicationUser> userManager,
+            ILocationUsersRepository locationUsersRepository,
+            ILocationActionEventsRepository locationActionEventsRepository,
+            ILocationRepository locationRepository,
+            ILogger<LocationsController> logger)
+            : base(userManager, logger)
+        {
+            _locationRepository = locationRepository;
+            _locationActionEventsRepository = locationActionEventsRepository;
+            _locationUsersRepository = locationUsersRepository;
+        }
 
-//        public async Task<IActionResult> Index()
-//        {
-//            try
-//            {
-//                var locations
-//                    = await _locationRepository.GetLocationsForUser();
+        //Locations/GetLocations
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> GetLocations()
+        {
+            try
+            {
+                var user = await GetUser();
 
-//                return View(locations);
-//            }
-//            catch (Exception ex)
-//            {
-//                TempData["ErrorMessage"] = ex.Message;
+                var locations = await _locationRepository.GetLocationsForUser(user);
 
-//                return RedirectToAction("Error", "Home");
-//            }
-//        }
+                return Ok(locations);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
 
-//        public IActionResult Create()
-//        {
-//            try
-//            {
-//                return View();
-//            }
-//            catch (Exception ex)
-//            {
-//                TempData["ErrorMessage"] = ex.Message;
+                return BadRequest(new ErrorResponse(ex));
+            }
+        }
 
-//                return RedirectToAction("Error", "Home");
-//            }
-//        }
+        //Locations/GetLocation/{id}
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> GetLocation(Guid id)
+        {
+            try
+            {
+                var user = await GetUser();
 
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public async Task<IActionResult> Create([Bind("Name,IsSilentAlarm")] Location location)
-//        {
-//            try
-//            {
-//                if (ModelState.IsValid)
-//                {
-//                    location.Id = Guid.NewGuid();
-//                    //location.Armed = false;
-//                    //location.Triggered = false;
-//                    location.Active = true;
+                var locations = await _locationRepository.GetLocationsForUser(user);
 
-//                    var user = await base.GetUser();
+                var location = locations.Single(l => l.Id == id);
 
-//                    location.ApplicationUserId = user.Id;
+                if (location == null)
+                {
+                    return BadRequest(new ErrorMessage(ErrorMessageResponses.UnableToFindLocation));
+                }
 
-//                    await _locationRepository.CreateLocation(_locationUsersRepository, location);
+                return Ok(location);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
 
-//                    TempData["CustomResponseAlert"] = CustomResponseAlert.GetStringResponse(ResponseStatusEnum.Success, $"{location.Name} created successfully.");
+                return BadRequest(new ErrorResponse(ex));
+            }
+        }
 
-//                    return RedirectToAction(nameof(Index));
-//                }
-//                return View(location);
-//            }
-//            catch (Exception ex)
-//            {
-//                TempData["ErrorMessage"] = ex.Message;
+        //Locations/CreateLocation
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> CreateLocation([FromBody] LocationViewModel locationViewModel)
+        {
+            try
+            {
+                var user = await GetUser();
 
-//                return RedirectToAction("Error", "Home");
-//            }
-//        }
+                var location = new Location
+                {
+                    Id = new Guid(),
+                    Name = locationViewModel.Name.ToTitleCase(),
+                    IsSilentAlarm = locationViewModel.IsSilentAlarm,
+                    ApplicationUserId = user.Id
+                };
 
-//        public async Task<IActionResult> Edit(Guid? id)
-//        {
-//            try
-//            {
-//                if (id == null)
-//                {
-//                    return NotFound();
-//                }
+                await _locationRepository.CreateLocation(_locationUsersRepository, location);
 
-//                var location
-//                    = await _locationRepository.GetLocationById((Guid)id);
+                return Ok(location);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
 
-//                if (location == null)
-//                {
-//                    return NotFound();
-//                }
+                return BadRequest(new ErrorResponse(ex));
+            }
+        }
 
-//                return View(location);
-//            }
-//            catch (Exception ex)
-//            {
-//                TempData["ErrorMessage"] = ex.Message;
+        //Locations/ArmLocation
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> ArmLocation([FromBody] LocationViewModel locationViewModel)
+        {
+            try
+            {
+                var location = await _locationRepository.GetLocationById(locationViewModel.Id);
 
-//                return RedirectToAction("Error", "Home");
-//            }
-//        }
+                var user = await GetUser();
 
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public async Task<IActionResult> Edit(Guid id, [Bind("Armed,IsSilentAlarm,Id")] Location location)
-//        {
-//            try
-//            {
-//                if (id != location.Id)
-//                {
-//                    return NotFound();
-//                }
+                await _locationRepository.ValidateLocationForUser(user, location);
 
-//                if (ModelState.IsValid)
-//                {
-//                    var armed = location.Armed;
+                if (locationViewModel.Armed)
+                {
+                    await _locationRepository.ArmLocation(location, user, _locationActionEventsRepository);
+                }
+                else
+                {
+                    await _locationRepository.DisarmLocation(location, user, _locationActionEventsRepository);
+                }
 
-//                    var dbLocation = await _locationRepository.GetLocationById(location.Id);
+                return Ok(location);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
 
-//                    if (dbLocation != null)
-//                    {
-//                        dbLocation.IsSilentAlarm = location.IsSilentAlarm;
+                return BadRequest(new ErrorResponse(ex));
+            }
+        }
 
-//                        if (armed)
-//                        {
-//                            await _locationRepository.ArmLocation(dbLocation);
+        //Locations/DeleteLocation/{id}
+        [HttpDelete]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> DeleteLocation(Guid id)
+        {
+            try
+            {
+                var location
+                    = await _locationRepository.GetLocationById(id);
 
-//                            TempData["CustomResponseAlert"] = CustomResponseAlert.GetStringResponse(ResponseStatusEnum.Success, $"{dbLocation.Name} successfully armed.");
-//                        }
-//                        else
-//                        {
-//                            await _locationRepository.DisarmLocation(dbLocation);
+                var user = await GetUser();
 
-//                            TempData["CustomResponseAlert"] = CustomResponseAlert.GetStringResponse(ResponseStatusEnum.Success, $"{dbLocation.Name} successfully disarmed.");
-//                        }
-//                    }
-//                    else
-//                    {
-//                        return NotFound();
-//                    }
+                if (!location.CheckUserAdmin(user))
+                {
+                    return BadRequest(new ErrorMessage(ErrorMessageResponses.UnAuthorizedModifyLocation));
+                }
 
-//                    return RedirectToAction(nameof(Index));
-//                }
-//                return View(location);
-//            }
-//            catch (Exception ex)
-//            {
-//                TempData["ErrorMessage"] = ex.Message;
+                await _locationRepository.DeleteLocation(location);
 
-//                return RedirectToAction("Error", "Home");
-//            }
-//        }
-//    }
-//}
+                return Ok(id);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+
+                return BadRequest(new ErrorResponse(ex));
+            }
+        }
+    }
+}
