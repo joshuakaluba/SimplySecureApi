09a1fd16eee155b83be8031be0bbfdbd0f5350f7
Joshua Kaluba <joshkaluba@gmail.com>
2019-03-12 21:43:24 -0400

    Formatting changes

--- SimplySecureApi.Web/MiddleWare/MaintainCorsHeadersMiddleware.cs
+++ SimplySecureApi.Web/MiddleWare/MaintainCorsHeadersMiddleware.cs
@@ -1,5 +1,5 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace SimplySecureApi.Web.MiddleWare
 {
@@ -9,32 +9,42 @@ namespace SimplySecureApi.Web.MiddleWare
         {
             _next = next;
         }
+
         private readonly RequestDelegate _next;
 
         public async Task Invoke(HttpContext httpContext)
         {
-            // Find and hold onto any CORS related headers ...
             var corsHeaders = new HeaderDictionary();
+
             foreach (var pair in httpContext.Response.Headers)
             {
-                if (!pair.Key.ToLower().StartsWith("access-control-")) { continue; } // Not CORS related
+                if (!pair.Key.ToLower().StartsWith("access-control-"))
+                {
+                    continue;
+                }
+
                 corsHeaders[pair.Key] = pair.Value;
             }
 
-            // Bind to the OnStarting event so that we can make sure these CORS headers are still included going to the client
-            httpContext.Response.OnStarting(o => {
-                var ctx = (HttpContext)o;
-                var headers = ctx.Response.Headers;
-                // Ensure all CORS headers remain or else add them back in ...
+            httpContext.Response.OnStarting(o =>
+            {
+                var context = (HttpContext)o;
+
+                var headers = context.Response.Headers;
+
                 foreach (var pair in corsHeaders)
                 {
-                    if (headers.ContainsKey(pair.Key)) { continue; } // Still there!
+                    if (headers.ContainsKey(pair.Key))
+                    {
+                        continue;
+                    }
+
                     headers.Add(pair.Key, pair.Value);
                 }
+
                 return Task.CompletedTask;
             }, httpContext);
 
-            // Call the pipeline ...
             await _next(httpContext);
         }
     }
