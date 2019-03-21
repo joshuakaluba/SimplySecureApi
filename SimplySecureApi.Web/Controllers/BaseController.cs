6aebd2fe47db2842ead9ee9042c171b113ae0284
Joshua Kaluba <joshkaluba@gmail.com>
2019-03-21 11:59:50 -0400

    Formatting changes

--- SimplySecureApi.Web/Controllers/BaseController.cs
+++ SimplySecureApi.Web/Controllers/BaseController.cs
@@ -1,31 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SimplySecureApi.Data.Models.Authentication;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SimplySecureApi.Web.Controllers
 {
-    public abstract class BaseController : Controller
+    public abstract class BaseController<T> : Controller
     {
         protected readonly UserManager<ApplicationUser> UserManager;
+        protected ILogger<T> Logger;
 
-        protected BaseController(UserManager<ApplicationUser> userManager)
+        protected BaseController(UserManager<ApplicationUser> userManager, ILogger<T> logger)
         {
-            this.UserManager = userManager;
+            UserManager = userManager;
+            Logger = logger;
         }
 
         protected string GetUserId()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
             return userId;
         }
 
         protected async Task<ApplicationUser> GetUser()
         {
             var user = await UserManager.FindByIdAsync(GetUserId());
-
             return user;
         }
     }
